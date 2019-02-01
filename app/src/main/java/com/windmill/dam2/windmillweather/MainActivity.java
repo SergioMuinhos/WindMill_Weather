a5db39f42400df4dfd1627c67dd7012467f5c6a1
u028254 <u028254@cajarural.com>
2019-02-01 12:44:50 +0100

    limpieza

--- app/src/main/java/com/windmill/dam2/windmillweather/MainActivity.java
+++ app/src/main/java/com/windmill/dam2/windmillweather/MainActivity.java
@@ -18,7 +18,6 @@ import android.support.v4.widget.NestedScrollView;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
