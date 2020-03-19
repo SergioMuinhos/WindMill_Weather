0b36fcce3a0ecc17f10f5fdcded875838133cf8a
SergioMuinhos <sergiomuinosgarcia@gmail.com>
2020-03-19 21:36:26 +0100

    añadida opcion de preferences usando SharedPreferences

---
+++ app/src/main/java/com/windmill/dam2/windmillweather/AlmacenPreferencias.java
@@ -0,0 +1,18 @@
+package com.windmill.dam2.windmillweather;
+
+import android.content.Context;
+
+public class AlmacenPreferencias {
+    private static String PREFERECNIAS="provincia";
+    private static Context context;
+    public AlmacenPreferencias (Context context){
+        this.context=context;
+    }
+
+    public void guardarPreferencias(){
+
+    }
+
+
+
+}
